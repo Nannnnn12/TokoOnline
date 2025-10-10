bb4ac4f0b2dc021bd6c26c4e101aeb7c9fecaeea
aiboyyahya <khalifahadnan354@gmail.com>
2025-10-10 22:12:08 +0700

    first commit

---
+++ app/Filament/Resources/Categories/Schemas/CategoryForm.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Filament\Resources\Categories\Schemas;
+
+use App\Models\Category;
+use Filament\Forms\Components\FileUpload;
+use Filament\Forms\Components\TextInput;
+use Filament\Schemas\Components\Utilities\Set;
+use Filament\Schemas\Schema;
+use Illuminate\Support\Str;
+
+class CategoryForm
+{
+    public static function configure(Schema $schema): Schema
+    {
+        return $schema
+            ->components([
+                TextInput::make('category_name')
+                    ->required()
+                    ->afterStateUpdated(function (Set $set, ?string $state) {
+                        $set('slug', Str::slug($state));
+                    }),
+                TextInput::make('slug')
+                    ->required(),
+                FileUpload::make('icon')
+                    ->image()
+                    ->default(null),
+            ]);
+    }
+}
